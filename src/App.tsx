0551ea4b3885293e5f0bc0441307f2d52c6e79da
Mikhail <mtionmikhail@gmail.com>
2022-11-14 00:55:07 +0300

    Change 3d model and add camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Canvas } from "@react-three/fiber";
-import { Sky, Environment, PerspectiveCamera, OrthographicCamera, OrbitControls, Stats,Bounds } from '@react-three/drei'
-import './App.scss';
-import { Tree } from './Models/Tree';
+import React, { useState, useRef, useEffect } from "react";
+import * as THREE from "three";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls, ContactShadows } from "@react-three/drei";
+import "./App.scss";
+import { Tree } from "./Models/Tree";
 
 function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 12]}}>
-        <OrbitControls />
-      <Tree/>
+    <Canvas shadows camera={{ position: [0, 0, 200], fov: 8 }}>
+      <Tree />
+      <OrbitControls maxDistance={250} minDistance={80} />
     </Canvas>
   );
 }
